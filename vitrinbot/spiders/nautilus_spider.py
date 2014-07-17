9480c3feafb39d49666bdd4cbcebceefe599ba2a
Mustafa Hasturk <mustafa.hasturk@yandex.com>
2014-07-17 18:08:38 +0300

    nautilus id generated with md5

--- vitrinbot/spiders/nautilus_spider.py
+++ vitrinbot/spiders/nautilus_spider.py
@@ -4,6 +4,7 @@ from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.selector import Selector
 from vitrinbot.items import ProductItem
 from vitrinbot.base import utils
+import hashlib
 
 removeCurrency = utils.removeCurrency
 getCurrency = utils.getCurrency
@@ -37,9 +38,9 @@ class NautilusSpider(CrawlSpider):
                                 '/login\.asp.*',
                                 '/sepet\.asp.*',
                                 '/hakkimizda\.asp.*',
-                                'yardim.asp\.*',
-                                'iletisim_formu\.asp.*',
-                                'musteri_hizmetleri\.asp.*'
+                                '/yardim.asp\.*',
+                                '/iletisim_formu\.asp.*',
+                                '/catinfo\.asp(\?brw).*'# @todo gereksiz sayfalar
                           ),
             ),
             callback='parse_item',
@@ -55,11 +56,11 @@ class NautilusSpider(CrawlSpider):
         if not sl.xpath(self.xpaths['check_page']):
             return i
 
-        i['id'] = self.product_id
+        i['id'] = hashlib.md5(response.url.encode('utf-8')).hexdigest()
         i['url'] = response.url
         i['category'] = " > ".join(sl.xpath(self.xpaths['category']).extract())
         i['title'] = sl.xpath(self.xpaths['title']).extract()[0].strip()
-        i['price'] = sl.xpath(self.xpaths['price']).extract()[0].strip()
+        i['special_price'] = i['price'] = sl.xpath(self.xpaths['price']).extract()[0].strip()
 
         images = []
         for img in sl.xpath(self.xpaths['images']).extract():
@@ -70,7 +71,7 @@ class NautilusSpider(CrawlSpider):
 
         i['brand'] = "Nautilus"
         
-        i['special_price']=i['expire_timestamp']=i['sizes']=i['colors'] = ''
+        i['expire_timestamp']=i['sizes']=i['colors'] = ''
 
         i['currency'] = sl.xpath(self.xpaths['currency']).extract()[0].strip()
 
