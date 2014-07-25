15e90527a3f72ae3bddccf03b2ceddf1aba0cb8f
Mustafa Hasturk <mustafa.hasturk@yandex.com>
2014-07-25 17:38:38 +0300

    requests module added, set_start_urls added

--- vitrinbot/spiders/milasorta.py
+++ vitrinbot/spiders/milasorta.py
@@ -1,29 +1,52 @@
 # -*- coding: utf-8 -*-
-
 from scrapy.contrib.linkextractors import LinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
-
+from scrapy.http import  Request
 from vitrinbot.items import ProductItem
 from scrapy.selector import Selector
 from vitrinbot.base import utils
-import re
 import urllib
+import re
+import requests
+from scrapy.http import TextResponse
+
+
+removeCurrency = utils.removeCurrency
+getCurrency = utils.getCurrency
+replaceCommaWithDot = utils.replaceCommaWithDot
 
 
 class MilasortaSpider(CrawlSpider):
     name = "milasorta"
     allowed_domains = ["www.milasorta.com"]
-    start_urls = (
-        'http://www.milasorta.com/',
-    )
+    start_urls = []
+    start_urls_ajax = 'http://www.milasorta.com/ajax/ajax_ulli.asp'
     xml_filename = 'milasorta-%d.xml'
 
     rules = (
-        Rule(LinkExtractor(allow=('\?kategori'))),
-        Rule(LinkExtractor(allow=('\?urun')), callback='parse_item',),
+        Rule(LinkExtractor(allow=('\?.*kategori'))),
+        Rule(LinkExtractor(allow=('\/\?.*urun')), callback='parse_item',),
     )
+    def __init__(self, category=None, *args, **kwargs):
+        super(MilasortaSpider, self).__init__(*args, **kwargs)
+        self.set_start_urls()
 
-    def parse(self, response):
+    def set_start_urls(self):
+
+        response = requests.get(self.start_urls_ajax)
+
+        textResponse = TextResponse(
+            response.url,
+            body=response.text,
+            encoding=response.encoding
+        )
+
+        s = Selector(textResponse)
+
+        for link in s.xpath('//a/@href').extract():
+            self.start_urls.append(link.replace('./','http://www.milasorta.com/'))
+
+    def parse_item(self, response):
         i = ProductItem()
         sl = Selector(response=response)
 
@@ -31,40 +54,47 @@ class MilasortaSpider(CrawlSpider):
 
         i['id'] = ''.join(sl.xpath('//input[@id="urun_id"]/@value').extract())
         i['title'] = ''.join(sl.xpath('//h1[@class="UrunBilgisiUrunAdi"]/text()').extract())
-        
-        i['price'] = utils.replaceCommaWithDot(''.join(
-            sl.xpath('//span[@class="UrunBilgisiIndirimsizFiyatiDivBaslik"]/following-sibling::p/text()').extract()))
-        i['special_price'] = utils.replaceCommaWithDot(''.join(
-            sl.xpath('//p[@id="UrunBilgisiIndirimliFiyatiDiv"]/text()').extract()))
 
-        i['description'] = ''.join(sl.xpath('//td[@class="UrunBilgisiUrunBilgiIcerikTd"]').extract())
-        i['currency'] = 'TL'
+        priceText = ''.join(
+            sl.xpath('//span[@class="UrunBilgisiIndirimsizFiyatiDivBaslik"]/following-sibling::p/text()').extract())
+        i['price'] = removeCurrency(priceText).replace(',','.')
 
-        sizes = []
-        regexSize = re.compile("([a-zA-Z]+)\s*\(")
-        if not sl.xpath('//td[@class="std_option3"]/text()').extract():
-            for sz in sl.xpath('//option/text()').extract()[1:]:
-                sizes.append(regexSize.findall(sz)[0]) if regexSize.search(sz) else sizes.append(sz)
-            i['sizes'] = sizes if sizes else ''
-            i['colors'] = []
+
+        if sl.xpath('//p[@div="UrunBilgisiIndirimliFiyatiDiv"]'):
+            i['special_price'] = removeCurrency(''.join(
+                sl.xpath('//p[@id="UrunBilgisiIndirimliFiyatiDiv"]/text()').extract())).replace(',','.')
         else:
-            for sz in sl.xpath('//td[@class="sto_option3"]//option/text()').extract()[1:]:
-                sizes.append(regexSize.findall(sz)[0]) if regexSize.search(sz) else sizes.append(sz)
-            i['sizes'] = sizes if sizes else ''
-            i['colors'] =  sl.xpath('//td[@class="sto_option2"]//option/text()').extract()[1:]
+            i['special_price'] = ''
 
-        #
-        # images = []
-        # for img in  sl.xpath('//img[@alt="imgBigPicture"]/@src').extract():
-        #     images.append(urllib.quote(('http://www.happymilk.com.tr'+img).encode('utf-8')))
-        # i['images'] = images
+
+
+        i['description'] = ''.join(sl.xpath('//td[@class="UrunBilgisiUrunBilgiIcerikTd"]//*/text()').extract())
+        i['currency'] = getCurrency(priceText)
+
+        i['category'] = ' > '.join(sl.xpath('//tr[@class="KategoriYazdirTabloTr"]//a/text()').extract()[1:])
+
+        # if ''.join(sl.xpath('//span[@class="UrunSecenekTabloSecenekAdi"]/'
+        #                     'text()').extract()).strip() == 'RENK SEÇENEĞİ'.decode('utf-8'):
+        #     colors = []
+        #     for cl in sl.xpath('//select[@class="UrunSecenekTabloSelectBox"]/option/text()').extract()[1:]:
+        #         colors.append(re.compile('-\s+([a-zA-Z]+)').findall(cl)[0])
+        #     i['colors'] = colors
+        # else:
+        #     i['colors'] = []
+
+        i['colors'] = ''
 
         images = []
-        for img in  sl.xpath('//img[@alt="imgBigPicture"]/@src').extract():
-            images.append('http://www.happymilk.com.tr'+urllib.quote(img.encode('utf-8')))
-        i['images'] = images
+        if sl.xpath('//td[@class="UrunBilgisiUrunResimSlaytTd"]'):
+            for img in  sl.xpath('//td[@class="UrunBilgisiUrunResimSlaytTd"]/div/a/@href').extract():
+                images.append('http://www.milasorta.com/'+urllib.quote(img.encode('utf-8')))
+            i['images'] = images
+        else:
+            i['images'] = ['http://www.milasorta.com/'+''.\
+                join(sl.xpath('//img[@class="UrunBilgisiUrunResmi"]/@src').extract())]
 
-        i['brand'] = 'Happy Milk'
-        i['category'] = i['expire_timestamp'] = ''
+        i['brand'] = 'Milasorta Accessories'
+        i['sizes'] = []
+        i['expire_timestamp'] = ''
 
         return i
