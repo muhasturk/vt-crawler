757160716b2f0d7d5c22b71d3d5f7ddd6a103d5e
Mesut Yılmaz <my@xant.org>
2014-09-17 17:02:01 +0300

    Zuzu spider

--- vitrinbot/spiders/zuzu.py
+++ vitrinbot/spiders/zuzu.py
@@ -8,9 +8,6 @@ import re
 
 from vitrinbot.base.spiders import VitrinSpider
 
-removeCurrency = utils.removeCurrency
-getCurrency =  utils.getCurrency
-
 class ZuzuSpider(VitrinSpider):
     name = 'zuzu'
     allowed_domains = ['zuzu.com']
@@ -19,33 +16,51 @@ class ZuzuSpider(VitrinSpider):
     xml_filename = 'zuzu-%d.xml'
 
     xpaths = {
-        'id':'//*[@class="UrunBilgisiUrunKodu"]/text()',
-        'check_page':'//h1[@class="UrunBilgisiUrunAdi"]'
-
+        'check_page':'//h1[@class="UrunBilgisiUrunAdi"]',
+        'product_id' :'//div[@class="sagAlan"]/p[@class="UrunBilgisiUrunKodu"]/text()',
+        'title': '//div[@class="sagAlan"]/h1[@class="UrunBilgisiUrunAdi"]/text()',
+        'description': 'concat(//div[@class="sagAlan"]/div[@class="UrunBilgisiUrunKisaAciklama"]/text(), '
+                       '\' <br> \', //div[@class="sagAlan"]//td[@class="UrunBilgisiUrunBilgiIcerikTd"]/text())',
+        'category': '//tr[@class="KategoriYazdirTabloTr"]/td//a/text()',
+        'images': '//div[@class="UrunBilgisiUrunKucukResim"]/a/@href',
+        'price': '//div[@class="sagAlan"]//p[@id="UrunBilgisiIndirimsizFiyatiDiv"]/text()',
     }
 
     rules = (
-        Rule(LinkExtractor(allow='[\w-]+'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow='[\w-]+', deny=('catinfo\.asp\?.*brw')), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        i = ProductItem()
-        sl = Selector(response=response)
+        product = ProductItem()
+        source = Selector(response)
 
-        if not sl.xpath(self.xpaths['check_page']):
-            return i
-        i['id'] = ("".join(sl.xpath(self.xpaths['id']).extract())).strip()
-        i['url'] = response.url
-        i['title'] = "".join(sl.xpath('//h1[@class="UrunBilgisiUrunAdi"]/text()').extract())
+        if not source.xpath(self.xpaths['check_page']):
+            return product
 
-        priceText = "".join(sl.xpath('//*[@id="UrunBilgisiIndirimsizFiyatiDiv"]/text()').extract())
-        i['price'] = removeCurrency(priceText)
-        i['currency'] = getCurrency(priceText)
+        product_id = source.xpath(self.xpaths['product_id']).extract()
+        title = source.xpath(self.xpaths['title']).extract()
+        description = source.xpath(self.xpaths['description']).extract()
+        category = source.xpath(self.xpaths['category']).extract()
+        images = source.xpath(self.xpaths['images']).extract()
+        price = source.xpath(self.xpaths['price']).extract()
 
-        i['description'] = "".join(sl.xpath('//*[@class="UrunBilgisiUrunBilgiIcerikTd"]//text()').extract())
+        product_images = []
+        for image in images:
+            if image.find('http://') != 0:
+                product_images.append('http://www.zuzu.com/' + image)
 
+        if price:
+            price = float(self.get_price(price[0]))
+        else:
+            price = 0
 
-        i['expire_timestamp']=i['special_price'] = i['brannd'] = i['sizes'] = ''
+        product['id'] = "".join(product_id).strip()
+        product['url'] = response.url
+        product['title'] = "".join(title).strip()
+        product['description'] = "".join(description).strip()
+        product['category'] = " > ".join(category[1:-1]).strip()
+        product['price'] = price
+        product['currency'] = 'TL'
+        product['images'] = product_images
 
-
-        return i
+        return product
